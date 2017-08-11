ef24ad4ba3fe5809f7b67225773c2107cc473021
porsch <porsch911@hotmail.fr>
2017-08-11 09:07:08 +0200

    final step

--- adneom/src/main/java/kata/fizzbuzz/Fizzbuzz.java
+++ adneom/src/main/java/kata/fizzbuzz/Fizzbuzz.java
@@ -3,7 +3,32 @@ package kata.fizzbuzz;
 public class Fizzbuzz {
 	
 	public static String resultatPour(int nombre) {
-		return "11";	
+		
+		if(nombre<=0) {
+			throw new IllegalArgumentException("nomber inférieur a 1");
+		}
+		
+		if (estMultipledeQuinze(nombre)) {
+			return "fizzbuzz";
+		}else if (estMultipledeTrois(nombre)) {
+			return "fizz";
+		}else if (estMultipledeCinq(nombre)) {
+			return "buzz";
+		}else {
+			return String.valueOf(nombre);	
+		}
+	}
+
+	private static boolean estMultipledeQuinze(int nombre) {
+		return nombre%15==0;
+	}
+
+	private static boolean estMultipledeTrois(int nombre) {
+		return nombre%3==0;
+	}
+
+	private static boolean estMultipledeCinq(int nombre) {
+		return nombre%5==0;
 	}
 
 }
--- adneom/src/test/java/kata/fizzbuzz/FizzbuzzTest.java
+++ adneom/src/test/java/kata/fizzbuzz/FizzbuzzTest.java
@@ -7,8 +7,54 @@ import org.junit.Test;
 public class FizzbuzzTest {
 	
 	@Test
-	public void RetourneUnCommeNombre() {
+	public void RetourneNombreSiUN() {
 		assertEquals(Fizzbuzz.resultatPour(1), "1");
 	}
+	
+	@Test
+	public void RetourneNombreSiDeux() {
+		assertEquals(Fizzbuzz.resultatPour(2), "2");
+	}
+	
+	@Test
+	public void RetourneFizzSiTrois() {
+		assertEquals(Fizzbuzz.resultatPour(3), "fizz");
+	}
 
+	@Test
+	public void RetourneFizzSiSix() {
+		assertEquals(Fizzbuzz.resultatPour(6), "fizz");
+	}
+	
+	@Test
+	public void RetourneFizzSiCinq() {
+		assertEquals(Fizzbuzz.resultatPour(5), "buzz");
+	}
+	
+	@Test
+	public void RetourneFizzSiDix() {
+		assertEquals(Fizzbuzz.resultatPour(10), "buzz");
+	}
+	
+	@Test
+	public void RetourneFizzBuzzSiQuinze() {
+		assertEquals(Fizzbuzz.resultatPour(15), "fizzbuzz");
+	}
+	
+	@Test
+	public void RetourneFizzBuzzSiTrente() {
+		assertEquals(Fizzbuzz.resultatPour(30), "fizzbuzz");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void RetourneExceptionSiZero() {
+		Fizzbuzz.resultatPour(0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void RetourneExceptionSiNegative() {
+		Fizzbuzz.resultatPour(-1);
+	}
+	
+	
 }
